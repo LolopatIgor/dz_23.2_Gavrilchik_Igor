b1d5e982a0c01a11cbe2fbf73ee816be8269af7e
Igor Gavrilchik <Lolopat1994@gmail.com>
2024-09-17 23:21:45 +0300

    initial

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list
+    }
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def product_detail(request, pk):
+    # Получаем объект продукта по его первичному ключу (pk)
+    product = Product.objects.get(pk=pk)
+    print(product.image)
+    # Контекст для шаблона
+
+    context = {
+        'product_item': product
+    }
+
+    # Рендерим шаблон product_detail.html с контекстом
+    return render(request, 'product_detail.html', context)
